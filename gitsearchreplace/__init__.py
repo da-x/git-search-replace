cc43601c2f7238ec93579e8e3f313f4882273b81
Erez Yaffe <erez.yaffe@elastifile.com>
2015-12-31 10:13:51 +0200

    Fix handling of duplicated files in git ls-files output

--- gitsearchreplace/__init__.py
+++ gitsearchreplace/__init__.py
@@ -110,8 +110,9 @@ class GitSearchReplace(object):
 
     def search_replace_in_files(self):
         filenames = run_subprocess(["git", "ls-files"]).splitlines()
+        unique_filenames = sorted(list(set(filenames)))
         filtered_filenames = []
-        for filename in filenames:
+        for filename in unique_filenames:
             excluded = False
             for exclude in self.exclude:
                 if fnmatch.fnmatch(filename, exclude):
